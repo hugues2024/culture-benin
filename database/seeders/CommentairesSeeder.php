a21dbc9a1d71063a5872e7f7e51db79eda7aad89
HOUNKPATIN Hugues <huguesmariehounkpatin1@gmail.com>
2025-12-08 21:33:39 +0100

    Modification encore du dockerfile pour les seeders

---
+++ database/seeders/CommentairesSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Commentaire;
+use App\Models\Contenu;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class CommentairesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $commentaires = [
+            // Commentaires sur le contenu ID=1 (conte Pokou)
+            [
+                'commentaire' => 'Excellente histoire ! La petite Pokou est un symbole de courage.',
+                'note' => 5,
+                'id_user' => 4, // Hugues Hounkpatin
+                'id_contenu' => 1,
+            ],
+            [
+                'commentaire' => 'Très beau conte traditionnel. Merci pour le partage !',
+                'note' => 4,
+                'id_user' => 5, // Dewanou Hounkpatin
+                'id_contenu' => 1,
+            ],
+            [
+                'commentaire' => 'J\'adore cette histoire, elle me rappelle mon enfance à Nikki.',
+                'note' => 5,
+                'id_user' => 2, // Hugue HOUNKPATIN
+                'id_contenu' => 1,
+            ],
+            // Commentaires sur le contenu ID=2 (Proverbe)
+            [
+                'commentaire' => 'Proverbe très sage. On l\'utilise souvent dans ma région.',
+                'note' => 5,
+                'id_user' => 1, // Manager Platform
+                'id_contenu' => 2,
+            ],
+        ];
+
+        foreach ($commentaires as $commentaire) {
+            Commentaire::create([
+                'commentaire' => $commentaire['commentaire'],
+                'note' => $commentaire['note'],
+                'id_user' => $commentaire['id_user'],
+                'id_contenu' => $commentaire['id_contenu'],
+                'created_at' => now()->subDays(rand(1, 30)),
+                'updated_at' => now()->subDays(rand(1, 30)),
+            ]);
+        }
+    }
+}
--- database/seeders/MediasSeeder.php
+++ database/seeders/MediasSeeder.php
@@ -1,21 +1,10 @@
 <?php
-
+// MediasSeeder.php
 namespace Database\Seeders;
-
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class MediasSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    <?php
-// MediasSeeder.php
-namespace Database\Seeders;
-
 use App\Models\Media;
-use Illuminate\Database\Seeder;
 
 class MediasSeeder extends Seeder
 {
@@ -28,19 +17,4 @@ class MediasSeeder extends Seeder
             'id_type_media' => 1
         ]);
     }
-}
-
-// ParlersSeeder.php
-class ParlersSeeder extends Seeder
-{
-    public function run(): void
-    {
-        \App\Models\Parler::create([
-            'id_langue' => 2, 'id_region' => 1
-        ]);
-    }
-}
-
-// CommentairesSeeder.php + PaiementsSeeder.php suivent la même logique
-
 }
